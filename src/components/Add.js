613d735af4064d860e208d19e13b1acc74be7c13
Kiernan <Kiernan1994@gmail.com>
2024-10-03 16:02:16 -0700

    content to the add file

---
+++ src/components/Add.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import BeanForm from "./BeanForm";
+import { v4 } from "uuid";
+
+
+function Add(props){
+
+  function addNewBeanToList(event){
+    event.preventDefault();
+    props.addNewBeanToList({
+      name: event.target.name.value,
+      recipe: event.target.recipe.value,
+      count: parseInt(event.target.count.value),
+      id: v4(),
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <BeanForm
+        formSubmissionHandler={addNewBeanToList}
+        buttonText="Help" />
+    </React.Fragment>
+  );
+}
+
+Add.propTypes = {
+  addNewBeanToList: PropTypes.func
+};
+
+export default Add;
